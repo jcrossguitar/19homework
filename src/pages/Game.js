8a927b991625948f2bbb4928caec07798b67f04b
jon <jcrossguitar@gmail.com>
2019-11-03 09:30:51 -0800

    I added pokemon to seedDB.js, added a jumbotron to index.js, i added style.css and game.js

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,46 +1,122 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import { Col, Row, Container } from "../components/Grid";
+import DeleteBtn from "../components/DeleteBtn";
 import Jumbotron from "../components/Jumbotron";
 import API from "../utils/API";
+import { Link } from "react-router-dom";
+import { Col, Row, Container } from "../components/Grid";
+import { List, ListItem } from "../components/List";
+import { Input, TextArea, FormBtn } from "../components/Form";
 
-class Detail extends Component {
-    state = {
-      book: {}
-    };
-    // When this component mounts, grab the book with the _id of this.props.match.params.id
-    // e.g. localhost:3000/books/599dcb67f0f16317844583fc
-    componentDidMount() {
-      API.getBook(this.props.match.params.id)
-        .then(res => this.setState({ book: res.data }))
+class Books extends Component {
+  state = {
+    books: [],
+    Pokemon: "",
+    author: "",
+    synopsis: ""
+  };
+
+  componentDidMount() {
+    this.loadGame();
+  }
+
+  loadGame = () => {
+    API.getBooks()
+      .then(res =>
+        this.setState({ books: res.data, Pokemon: "", author: "", synopsis: "" })
+      )
+      .catch(err => console.log(err));
+  };
+
+  deleteBook = id => {
+    API.deleteBook(id)
+      .then(res => this.loadGame())
+      .catch(err => console.log(err));
+  };
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    if (this.state.Pokemon && this.state.author) {
+      API.saveBook({
+        Pokemon: this.state.Pokemon,
+        author: this.state.author,
+        synopsis: this.state.synopsis
+      })
+        .then(res => this.loadGame())
         .catch(err => console.log(err));
     }
+  };
 
-render() {
+  render() {
     return (
       <Container fluid>
         <Row>
-          <Col size="md-12">
+          <Col size="md-6">
             <Jumbotron>
-              <h1>
-                {this.state.book.title} by {this.state.book.author}
-              </h1>
+              <h1>Memory Game</h1>
             </Jumbotron>
+            <form>
+
+                <div class="card">
+                <img src="img_avatar.png" alt="Avatar" style="width:100%">
+                <div class="container">
+                <h4><b>John Doe</b></h4> 
+                <p>Architect & Engineer</p> 
+                </div>
+                </div>
+            <Input
+
+                value={this.state.Pokemon}
+                onChange={this.handleInputChange}
+                name="Pokemon"
+                placeholder="Pokemon (required)"
+              />
+              <Input
+                value={this.state.author}
+                onChange={this.handleInputChange}
+                name="author"
+                placeholder="Author (required)"
+              />
+              <TextArea
+                value={this.state.synopsis}
+                onChange={this.handleInputChange}
+                name="synopsis"
+                placeholder="Synopsis (Optional)"
+              />
+              <FormBtn
+                disabled={!(this.state.author && this.state.Pokemon)}
+                onClick={this.handleFormSubmit}
+              >
+                Submit Book
+              </FormBtn>
+            </form>
           </Col>
-        </Row>
-        <Row>
-          <Col size="md-10 md-offset-1">
-            <article>
-              <h1>Synopsis</h1>
-              <p>
-                {this.state.book.synopsis}
-              </p>
-            </article>
-          </Col>
-        </Row>
-        <Row>
-          <Col size="md-2">
-            <Link to="/">← Back to Authors</Link>
+          <Col size="md-6 sm-12">
+            <Jumbotron>
+              <h1>Do not click on the same image twice!</h1>
+            </Jumbotron>
+            {this.state.books.length ? (
+              <List>
+                {this.state.books.map(book => (
+                  <ListItem key={book._id}>
+                    <Link to={"/books/" + book._id}>
+                      <strong>
+                        {book.Pokemon} by {book.author}
+                      </strong>
+                    </Link>
+                    <DeleteBtn onClick={() => this.deleteBook(book._id)} />
+                  </ListItem>
+                ))}
+              </List>
+            ) : (
+              <h3>No Results to Display</h3>
+            )}
           </Col>
         </Row>
       </Container>
@@ -48,4 +124,4 @@ render() {
   }
 }
 
-export default Detail;
+export default Books;
